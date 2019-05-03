66cc1f448f97aaae27f7b28b3ee7451a12fa063e
muhammad-magdi <muhammad.magdi1024@gmail.com>
2019-05-03 19:15:02 +0200

    Implemented the MonotonicStack Methods.

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt(), x;
-        List<Integer> myList = new ArrayList<>();
-        for(int i = 0 ; i < n ; ++i) {
-            x = in.nextInt();
-            myList.addBack(x);
-        }
-        myList.removeBack();
-        myList.removeBack();
-        for(int i = 0 ; i < myList.size() ; ++i){
-            System.out.println(myList.get(i));
-        }
+        MonotonicStack<Integer> st = new MonotonicStack<>();
+        st.push(7);
+        st.push(2);
+        st.push(1);
+        st.push(6);
+        System.out.println(st.getMax());
+        System.out.println(st.getMin());
+        st.pop();
+        System.out.println(st.getMax());
+        System.out.println(st.getMin());
+        st.pop();
+        System.out.println(st.getMax());
+        System.out.println(st.getMin());
+        st.pop();
+        System.out.println(st.getMax());
+        System.out.println(st.getMin());
     }
 }
--- src/MonotonicStack.java
+++ src/MonotonicStack.java
@@ -1,29 +1,44 @@
-public class MonotonicStack<E> {
+public class MonotonicStack<E extends Comparable<E>>{
+    private Stack<E> stack, maximums, minimums;
+
+    MonotonicStack(){
+        stack = new ArrayStack<>();
+        maximums = new ArrayStack<>();
+        minimums = new ArrayStack<>();
+    }
+
     public void push(E data){
-        //TODO
+        stack.push(data);
+        if(maximums.isEmpty())
+            maximums.push(data);
+        else if(data.compareTo(maximums.top()) >= 0)
+            maximums.push(data);
+
+        if(minimums.isEmpty())
+            minimums.push(data);
+        else if(data.compareTo(minimums.top()) <= 0)
+            minimums.push(data);
     }
     public E getMax(){
-        //TODO
-        return null;
+        return maximums.top();
     }
     public E getMin(){
-        //TODO
-        return null;
+        return minimums.top();
     }
     public E pop(){
-        //TODO
-        return null;
+        if(stack.top().compareTo(minimums.top()) == 0)
+            minimums.pop();
+        if(stack.top().compareTo(maximums.top()) == 0)
+            maximums.pop();
+        return stack.pop();
     }
     public E top(){
-        //TODO
-        return null;
+        return stack.top();
     }
     public int size(){
-        //TODO
-        return 0;
+        return stack.size();
     }
     public boolean isEmpty(){
-        //TODO
-        return false;
+        return stack.isEmpty();
     }
 }
