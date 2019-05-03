f65c407522ce36ce223aa41ae120ab97a9dcd0af
muhammad-magdi <muhammad.magdi1024@gmail.com>
2019-05-03 19:18:20 +0200

    Added The MonotonicQueue Class Methods.

---
+++ src/MonotonicQueue.java
@@ -0,0 +1,30 @@
+public class MonotonicQueue<E extends Comparable<E>> {
+
+    public void push(E data){
+        //TODO
+    }
+    public E front(){
+        //TODO
+        return null;
+    }
+    public E pop(){
+        //TODO
+        return null;
+    }
+    public E getMin(){
+        //TODO
+        return null;
+    }
+    public E getMax(){
+        //TODO
+        return null;
+    }
+    public int size(){
+        //TODO
+        return 0;
+    }
+    public boolean isEmpty(){
+        //TODO
+        return false;
+    }
+}
